1a7acd7d11655ed81c5a849f8c9bd2e95d4961ba
Frankie Roberto <frankie@frankieroberto.com>
2020-02-10 12:35:35 +0000

    Add skip link helper

---
+++ app/helpers/govuk_design_system/skip_link_helper.rb
@@ -0,0 +1,14 @@
+module GovukDesignSystem
+  module SkipLinkHelper
+    # Based on https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/components/skip-link/template.njk
+    def govukSkipLink(text: nil, html: nil, href: "#content", classes: "", attributes: {})
+
+      attributes["class"] = "govuk-skip-link #{classes}"
+      attributes["href"] = href
+
+      content_tag("a", attributes) do
+        html || text
+      end
+    end
+  end
+end
