79ea2683f599937694cabfc84bcf3b7cde17d361
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-25 14:38:01 +0100

    Add fieldset component in erb

--- app/helpers/govuk_design_system/fieldset_helper.rb
+++ app/helpers/govuk_design_system/fieldset_helper.rb
@@ -1,30 +1,9 @@
 module GovukDesignSystem
   # Based on https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/components/fieldset/template.njk
   module FieldsetHelper
-    # rubocop:disable Lint/UnusedMethodArgument
-    # TODO: implement and make use of describedBy
-    def govukFieldset(classes: "", describedBy: "", legend: {}, attributes: {}, &block)
-      fieldset_attributes = {
-        class: "govuk-fieldset",
-        "aria-describedby" => describedBy
-      }
-      tag.fieldset(**fieldset_attributes) do
-        legend = capture do
-          tag.legend class: "govuk-fieldset__legend #{legend[:classes]}" do
-            if legend[:isPageHeading]
-              tag.h1 class: "govuk-fieldset__heading" do
-                legend[:html].presence || legend[:text]
-              end
-            else
-              legend[:html].presence || legend[:text]
-            end
-          end
-        end
-        content = capture(&block)
-
-        legend + content
-      end
+    def govukFieldset(kwargs = {}, &block)
+      kwargs.merge!(block: block)
+      render "components/govuk_fieldset", **kwargs
     end
-    # rubocop:enable Lint/UnusedMethodArgument
   end
 end
