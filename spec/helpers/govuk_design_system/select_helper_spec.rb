6adb67a28bac915fdefa2476e68d709bc1d156bd
stdstaff <85587097+stdstaff@users.noreply.github.com>
2022-09-15 09:08:46 +0100

    Fix attributes merge for select

--- spec/helpers/govuk_design_system/select_helper_spec.rb
+++ spec/helpers/govuk_design_system/select_helper_spec.rb
@@ -44,5 +44,42 @@ RSpec.describe GovukDesignSystem::SelectHelper, type: :helper do
         </div>
       HTML
     end
+
+    it "allows attributes to be passed" do
+      html = helper.govukSelect({
+        id: "sort",
+        name: "sort",
+        label: {
+          text: "Sort by"
+        },
+        items: [
+          {
+            value: "published",
+            text: "Recently published"
+          },
+          {
+            value: "updated",
+            text: "Recently updated",
+            selected: true
+          }
+        ],
+        attributes: {
+          disabled: true,
+          data: { test: "testing" }
+        }
+      })
+
+      expect(html).to match_html(<<~HTML)
+        <div class="govuk-form-group">
+          <label class="govuk-label" for="sort">
+            Sort by
+          </label>
+          <select class="govuk-select" id="sort" name="sort" disabled="disabled" data-test="testing">
+            <option value="published">Recently published</option>
+            <option value="updated" selected="selected">Recently updated</option>
+          </select>
+        </div>
+      HTML
+    end
   end
 end
