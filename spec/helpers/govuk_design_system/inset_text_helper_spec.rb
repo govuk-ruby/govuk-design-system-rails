ca1ad3ef74212d1b6a1a52d58daccab901962c21
Kyle MacPherson <kaimac2010@gmail.com>
2022-10-19 14:52:53 +0100

    Add govuk_inset_text component

---
+++ spec/helpers/govuk_design_system/inset_text_helper_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+RSpec.describe GovukDesignSystem::InsetTextHelper, type: :helper do
+  describe "#govukInsetText" do
+    it "returns the correct HTML for the default example" do
+      html = helper.govukInsetText({
+        text: "It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application."
+      })
+
+      expect(html).to match_html(<<~HTML)
+        <div class="govuk-inset-text">
+          It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.
+        </div>
+      HTML
+    end
+
+    it "returns the correct HTML for an example using html" do
+      inset_html = '<p class="govuk-body-l govuk-!-margin-bottom-4">
+                            Creating a case starts from a product record page.
+                        </p>
+                        <p class="govuk-body">
+                            Find a product and create the case from there.
+                        </p>
+                        <details class="govuk-details" data-module="govuk-details">
+                            <summary class="govuk-details__summary">
+                              <span class="govuk-details__summary-text">
+                                How to create a case
+                              </span>
+                            </summary>
+                        </details>'.html_safe
+
+      html = helper.govukInsetText({
+        classes: "govuk-!-margin-bottom-7",
+        html: inset_html
+      })
+
+      expect(html).to match_html(<<~HTML)
+       <div class="govuk-inset-text govuk-!-margin-bottom-7">
+            <p class="govuk-body-l govuk-!-margin-bottom-4">
+                Creating a case starts from a product record page.
+            </p>
+            <p class="govuk-body">
+                Find a product and create the case from there.
+            </p>
+            <details class="govuk-details" data-module="govuk-details">
+                <summary class="govuk-details__summary">
+                  <span class="govuk-details__summary-text">
+                    How to create a case
+                  </span>
+                </summary>
+            </details>
+        </div>
+      HTML
+    end
+  end
+end
