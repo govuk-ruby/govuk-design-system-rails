ee38ff94fb9013af484863606b7f4fac83ea690f
Slawosz Slawinski <slawosz@gmail.com>
2023-02-01 15:05:03 +0000

    Allow extra html in radio buttons

--- app/helpers/govuk_design_system/radios_helper.rb
+++ app/helpers/govuk_design_system/radios_helper.rb
@@ -1,7 +1,8 @@
 module GovukDesignSystem
   module RadiosHelper
-    def govukRadios(kwargs = {})
+    def govukRadios(kwargs = {}, &blk)
       form = kwargs[:form]
+      kwargs[:extra_html] = capture(&blk) if block_given?
       render((form.present? ? "form_components/govuk_radios" : "components/govuk_radios"), **kwargs)
     end
   end
